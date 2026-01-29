6b5871320078525fea53c67a74c47fbacad28023
isaackps <isaac.kps@gmail.com>
2026-01-29 15:02:45 +0800

    feat: update with SUTD templates

---
+++ src/templates/SUTD5Template/index.js
@@ -0,0 +1,12 @@
+
+import SUTDTranscript from "./transcript";
+
+const templates = [
+  {
+    id: "transcript",
+    label: "Transcript",
+    template: SUTDTranscript
+  }
+];
+
+export default templates;
---
+++ src/templates/SUTDTemplate/index.js
@@ -0,0 +1,17 @@
+import SUTDCert from "./certificate";
+import SUTDTranscript from "./transcript";
+
+const templates = [
+  {
+    id: "certificate",
+    label: "Certificate",
+    template: SUTDCert
+  },
+  {
+    id: "transcript",
+    label: "Transcript",
+    template: SUTDTranscript
+  }
+];
+
+export default templates;
