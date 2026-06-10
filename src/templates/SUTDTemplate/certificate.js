a02135fa943fa1d437ff1173fb9bf65a26e8791c
opencert-sutd <52150238+opencert-sutd@users.noreply.github.com>
2026-06-10 10:06:44 +0800

    Update certificate.js

--- src/templates/SUTDTemplate/certificate.js
+++ src/templates/SUTDTemplate/certificate.js
@@ -105,13 +105,13 @@ const centerRowStyle = {
 };
 
 const chairImgStyle = {
-  width: "200px",
+  width: "230px",
   height: "60px",
   borderBottom: "1px solid black"
 };
 
 const presidentImgStyle = {
-  width: "200px",
+  width: "230px",
   height: "60px",
   borderBottom: "1px solid black"
 };
@@ -156,7 +156,7 @@ const Template = ({ document }) => {
   const signatureSectionStyle = {
     display: "flex",
     justifyContent: "space-between",
-    width: "820px",
+    width: "950px",
     margin: `${signatureTopMargin}px auto 0 auto`
   };
 
@@ -246,7 +246,7 @@ const Template = ({ document }) => {
         </div>
 
         <div style={signatureSectionStyle}>
-          <div style={{ width: "220px", textAlign: "center" }}>
+          <div style={{ width: "260px", textAlign: "center" }}>
             <img
               src={document.additionalData.certSignatories[0].signature}
               style={chairImgStyle}
@@ -259,7 +259,7 @@ const Template = ({ document }) => {
             </div>
           </div>
 
-          <div style={{ width: "220px", textAlign: "center" }}>
+          <div style={{ width: "260px", textAlign: "center" }}>
             <img
               src={document.additionalData.certSignatories[1].signature}
               style={presidentImgStyle}
