accc994e6eeed77607fc234da315f25bda03119a
opencert-sutd <52150238+opencert-sutd@users.noreply.github.com>
2026-06-12 20:51:39 +0800

    Update certificate.js

--- src/templates/SUTDDuelTemplate/certificate.js
+++ src/templates/SUTDDuelTemplate/certificate.js
@@ -74,7 +74,7 @@ const outerWrapperStyle = {
 
 const certificateStyle = {
   width: "1120px",
-  height: "830px",
+  height: "800px",
   border: "1px solid black",
   backgroundPosition: "1px",
   backgroundRepeat: "repeat",
